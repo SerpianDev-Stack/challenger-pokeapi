3eda89c2e4a81129c53052f823e6ba75df050de6
SerpianDev-Stack <joaolucasdomingos05@gmail.com>
2025-09-12 17:30:37 -0300

    ajustando rotas

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -4,15 +4,12 @@ import { PokeList } from "./pokeList";
 import { PokeDetails } from "./pokeDetails";
 import { useState } from "react";
 
-
-
 const AppRoutes = () => {
-
     const [showExtra, setShowExtra] = useState(false);
     const togglePokemons = () => setShowExtra(prev => !prev);
 
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/challenger-pokeapi/">
             <NavBar togglePokemons={togglePokemons} showExtra={showExtra} />
             <Routes>
                 <Route path="/" element={<PokeList showExtra={showExtra} />} />
